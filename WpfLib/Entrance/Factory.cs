820e9ae20c588afe72171680379e45d875e2da23
Feast <1353216996@qq.com>
2022-09-28 11:29:18 +0800

    Update

--- WpfLib/Entrance/Factory.cs
+++ WpfLib/Entrance/Factory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using Castle.MicroKernel.Registration;
+﻿using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using WpfLib.Helpers;
 using WpfLib.Helpers.Implement;
---
+++ WpfLib/Helpers/IHaveBackground.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfLib.Helpers
+{
+    public interface IHaveBackground : ICanChangeColor
+    {
+        Brush Background { get; set; }
+    }
+    public abstract class BackgroundBase : IHaveBackground
+    {
+        public abstract UIElement Subject { get; }
+        public Brush Color { get => Background; set => Background = value;
+        }
+        public abstract Brush Background { get; set; }
+    }
+    public class PanelBackground : BackgroundBase
+    {
+        public readonly Panel Panel;
+        public PanelBackground(Panel panel)
+        {
+            Panel = panel;
+        }
+
+        public override UIElement Subject => Panel;
+        public override Brush Background { get => Panel.Background; set => Panel.Background = value; }
+    }
+    public class BorderBackground : BackgroundBase
+    {
+        public readonly Border Border;
+        public BorderBackground(Border border)
+        {
+            Border = border;
+        }
+
+        public override UIElement Subject => Border;
+        public override Brush Background { get => Border.Background; set => Border.Background = value; }
+    }
+    public class ControlBackground : BackgroundBase
+    {
+        public readonly Control Control;
+        public ControlBackground(Control contentControl)
+        {
+            Control = contentControl;
+        }
+
+        public override UIElement Subject => Control;
+        public override Brush Background { get => Control.Background; set => Control.Background = value; }
+    }
+
+    public class TextBlockBackground : BackgroundBase
+    {
+        public override UIElement Subject => TextBlock;
+
+        public override Brush Background
+        {
+            get => TextBlock.Background;
+            set => TextBlock.Background = value;
+        }
+
+        public readonly TextBlock TextBlock;
+
+        public TextBlockBackground(TextBlock element)
+        {
+            TextBlock = element;
+        }
+    }
+}
