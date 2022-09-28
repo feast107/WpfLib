65bce30d7ec2a649cdeb2e1b708e36f72b4951fd
Feast <1353216996@qq.com>
2022-09-28 11:29:30 +0800

    new definitions

---
+++ WpfLib/Helpers/IHaveForeground.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace WpfLib.Helpers
+{
+    public interface IHaveForeground : ICanChangeColor
+    {
+        Brush Foreground { get; set; }
+    }
+
+    public abstract class ForegroundBase : IHaveForeground
+    {
+        public abstract UIElement Subject { get; }
+        public Brush Color { get => Foreground; set => Foreground = value; }
+        public abstract Brush Foreground { get; set; }
+    }
+    
+    public class TextBlockForeground : ForegroundBase
+    {
+        public override UIElement Subject => _subject;
+        public override Brush Foreground { get => _subject.Foreground; set=>_subject.Foreground = value; }
+        
+        private readonly TextBlock _subject;
+        public TextBlockForeground(TextBlock element)
+        {
+            _subject = element;
+        }
+
+    }
+
+    public class ButtonBaseForeground : ForegroundBase
+    {
+        public override UIElement Subject => _subject;
+        public override Brush Foreground { get => _subject.Background; set => _subject.Background = value; }
+        private readonly ButtonBase _subject;
+        public ButtonBaseForeground(ButtonBase element)
+        {
+            _subject = element;
+        }
+
+    }
+}
---
+++ WpfLib/Helpers/IHaveUi.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfLib.Helpers
+{
+    public interface IHaveUi
+    {
+        UIElement Subject { get; }
+    }
+
+    public interface ICanChangeColor : IHaveUi
+    {
+        Brush Color { get; set; }
+    }
+}
